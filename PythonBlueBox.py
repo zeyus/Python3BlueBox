345d8b2d0ff3e4a6cd0503c2768b60c27c430f05
Luke Ring <luke.ring@vu.edu.au>
2015-05-29 13:27:33 +1000

    Oops

--- PythonBlueBox.py
+++ PythonBlueBox.py
@@ -26,22 +26,22 @@ user_tones = {
 op_freq = [700.0, 900.0, 1100.0, 1300.0, 1300.0, 1500.0, 1700.0]
 
 op_tones = {
-    '1': (user_freq[0], user_freq[1]),
-    '2': (user_freq[0], user_freq[2]),
-    '3': (user_freq[1], user_freq[2]),
-    '4': (user_freq[0], user_freq[3]),
-    '5': (user_freq[1], user_freq[3]),
-    '6': (user_freq[2], user_freq[3]),
-    '7': (user_freq[0], user_freq[4]),
-    '8': (user_freq[1], user_freq[4]),
-    '9': (user_freq[2], user_freq[4]),
-    '0': (user_freq[3], user_freq[4]),  # 0 or "10"
-    'A': (user_freq[3], user_freq[4]),  # 0 or "10"
-    'B': (user_freq[0], user_freq[5]),  # 11 or ST3
-    'C': (user_freq[1], user_freq[5]),  # 12 or ST2
-    'D': (user_freq[2], user_freq[5]),  # KP
-    'E': (user_freq[3], user_freq[5]),  # KP2
-    'F': (user_freq[4], user_freq[5]),  # ST
+    '1': (op_freq[0], op_freq[1]),
+    '2': (op_freq[0], op_freq[2]),
+    '3': (op_freq[1], op_freq[2]),
+    '4': (op_freq[0], op_freq[3]),
+    '5': (op_freq[1], op_freq[3]),
+    '6': (op_freq[2], op_freq[3]),
+    '7': (op_freq[0], op_freq[4]),
+    '8': (op_freq[1], op_freq[4]),
+    '9': (op_freq[2], op_freq[4]),
+    '0': (op_freq[3], op_freq[4]),  # 0 or "10"
+    'A': (op_freq[3], op_freq[4]),  # 0 or "10"
+    'B': (op_freq[0], op_freq[5]),  # 11 or ST3
+    'C': (op_freq[1], op_freq[5]),  # 12 or ST2
+    'D': (op_freq[2], op_freq[5]),  # KP
+    'E': (op_freq[3], op_freq[5]),  # KP2
+    'F': (op_freq[4], op_freq[5]),  # ST
 }
 
 sr = 44100
